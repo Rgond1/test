1c9e53a636655b26a6a6f3ce5aac2595ddb53c55
Rgond1 <ravi120gond@gmail.com>
2024-07-17 01:11:37 +0530

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 // Example path to script.bat
-const scriptPath = '/var/task/script.bat';
+const scriptPath = '/var/task/script.sh';
 
 exec(scriptPath, (error, stdout, stderr) => {
     if (error) {
